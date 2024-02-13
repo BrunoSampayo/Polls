635ed41a80df8af5677710299cf9670034e8cc52
Bruno <brunosampayo1040@gmail.com>
2024-02-13 10:36:52 -0300

    feat: fastify Cokkie add

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,12 +1,21 @@
 import fastify from "fastify";
+import cookie from '@fastify/cookie'
 import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
 
 
 const app = fastify();
   
+app.register(cookie,{
+  secret: "20ace564-b141-4276-9789-8ed5c62632d9",
+  hook: 'onRequest',
+});
 
-app.register(createPoll)
-
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPoll);
+ 
 app.listen({port: 3333},()=>{
     console.log("Server is running on port 3333")
 })
